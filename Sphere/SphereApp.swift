4f40915927e4e222b8b98ac3bf89332f98b1e82d
Andyfer004 <99309349+Andyfer004@users.noreply.github.com>
2023-09-30 23:47:52 -0600

    agregar archivos

---
+++ Sphere/SphereApp.swift
@@ -0,0 +1,17 @@
+//
+//  SphereApp.swift
+//  Sphere
+//
+//  Created by Andy Fernando Fuentes Velásquez on 30/09/23.
+//
+
+import SwiftUI
+
+@main
+struct SphereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
